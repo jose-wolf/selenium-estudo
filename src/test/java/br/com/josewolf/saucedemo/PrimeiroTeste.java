3e34e394f902e8cd5f41561f0af9ef9171d71821
José Wolf <wolf.jose89@gmail.com>
2026-08-06 12:25:47 -0300

    Estudando sobre browser firefox

---
+++ src/test/java/br/com/josewolf/saucedemo/PrimeiroTeste.java
@@ -0,0 +1,45 @@
+package br.com.josewolf.saucedemo;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PrimeiroTeste {
+
+    @Test
+    void deveAbrirSauceDemo() {
+
+        FirefoxOptions options = new FirefoxOptions();
+
+        options.setBinary(
+                "/snap/firefox/current/usr/lib/firefox/firefox"
+        );
+
+        options.addArguments("-headless");
+
+        WebDriver navegador = new FirefoxDriver(options);
+
+
+        try {
+            navegador.get("https://www.saucedemo.com/");
+
+
+            String tituloDaPagina = navegador.getTitle();
+
+            navegador.findElement(By.name("user-name"));
+            navegador.findElement(By.name("login-button"));
+
+
+            assertEquals("Swag Labs", tituloDaPagina);
+        } finally {
+            navegador.quit();
+        }
+    }
+}
