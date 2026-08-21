33a0e3127a8cadaf6bd20347d7788f02e81e732f
José Wolf <wolf.jose89@gmail.com>
2026-08-21 15:16:01 -0300

    Praticando Navigate

--- src/test/java/br/com/josewolf/saucedemo/PrimeiroTeste.java
+++ src/test/java/br/com/josewolf/saucedemo/PrimeiroTeste.java
@@ -47,68 +47,72 @@ class PrimeiroTeste {
 
             botao.click();
 
-            WebElement tituloProducts = wait.until(
-                    ExpectedConditions.visibilityOfElementLocated(
-                            By.className("title")
-                    )
-            );
-
-            System.out.println("Título da página: " + tituloProducts.getText());
-            assertEquals("Products", tituloProducts.getText());
-            System.out.println("classe da página: " + tituloProducts.getAttribute("class"));
-
-            navegador.findElement(By.className("shopping_cart_link"));
-            navegador.findElement(By.id("react-burger-menu-btn"));
-            navegador.findElement(By.cssSelector("[data-test='social-linkedin']"));
-            navegador.findElement(By.tagName("a"));
-            navegador.findElement(By.linkText("Facebook"));
-
-
-            List<WebElement> produtos = navegador.findElements(By.cssSelector("[data-test='inventory-item']"));
-
-            System.out.println(produtos.size());
-            assertEquals(6, produtos.size());
-
-
-            WebElement addProduct = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                    By.cssSelector("[data-test='add-to-cart-sauce-labs-backpack']")
-            ));
-            boolean visivel = addProduct.isDisplayed();
-            System.out.println(visivel);
-            boolean botaoInterativo  = addProduct.isEnabled();
-            System.out.println(botaoInterativo);
-            boolean botaoSelecionado = addProduct.isSelected();
-            System.out.println(botaoSelecionado);
-            addProduct.click();
-
-
-
-            WebElement badgeCarrinho = navegador.findElement(
-                    By.cssSelector("[data-test='shopping-cart-badge']")
-            );
-            System.out.println("Itens no carrinho: " + badgeCarrinho.getText());
-            assertEquals("1", badgeCarrinho.getText());
-
-            WebElement removeProduct = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                    By.cssSelector("[data-test='remove-sauce-labs-backpack']")
-            ));
-            removeProduct.click();
-
-            WebElement linkedinLink = navegador.findElement(
-                    By.cssSelector("[data-test='social-linkedin']")
-            );
-            String linkedDestino = linkedinLink.getAttribute("href");
-            System.out.println(linkedDestino);
-            String tagName = linkedinLink.getTagName();
-            assertEquals(tagName, "a");
-
-            WebElement facebookLink = navegador.findElement(
-                    By.cssSelector("[data-test='social-facebook']")
-            );
-            String facebookDestino = facebookLink.getAttribute("href");
-            System.out.println(facebookDestino);
-            tagName = facebookLink.getTagName();
-            assertEquals(tagName, "a");
+            navegador.navigate().back();
+            navegador.navigate().refresh();
+//            WebElement tituloProducts = wait.until(
+//                    ExpectedConditions.visibilityOfElementLocated(
+//                            By.className("title")
+//                    )
+//            );
+//
+//            System.out.println("Título da página: " + tituloProducts.getText());
+//            assertEquals("Products", tituloProducts.getText());
+//            System.out.println("classe da página: " + tituloProducts.getAttribute("class"));
+//
+//            navegador.findElement(By.className("shopping_cart_link"));
+//            navegador.findElement(By.id("react-burger-menu-btn"));
+//            navegador.findElement(By.cssSelector("[data-test='social-linkedin']"));
+//            navegador.findElement(By.tagName("a"));
+//            navegador.findElement(By.linkText("Facebook"));
+//
+//
+//            List<WebElement> produtos = navegador.findElements(By.cssSelector("[data-test='inventory-item']"));
+//
+//            System.out.println(produtos.size());
+//            assertEquals(6, produtos.size());
+//
+//
+//            WebElement addProduct = wait.until(ExpectedConditions.visibilityOfElementLocated(
+//                    By.cssSelector("[data-test='add-to-cart-sauce-labs-backpack']")
+//            ));
+//            boolean visivel = addProduct.isDisplayed();
+//            System.out.println(visivel);
+//            boolean botaoInterativo  = addProduct.isEnabled();
+//            System.out.println(botaoInterativo);
+//            boolean botaoSelecionado = addProduct.isSelected();
+//            System.out.println(botaoSelecionado);
+//            addProduct.click();
+//
+//
+//
+//            WebElement badgeCarrinho = navegador.findElement(
+//                    By.cssSelector("[data-test='shopping-cart-badge']")
+//            );
+//            System.out.println("Itens no carrinho: " + badgeCarrinho.getText());
+//            assertEquals("1", badgeCarrinho.getText());
+//
+//            WebElement removeProduct = wait.until(ExpectedConditions.visibilityOfElementLocated(
+//                    By.cssSelector("[data-test='remove-sauce-labs-backpack']")
+//            ));
+//            removeProduct.click();
+//
+//            WebElement linkedinLink = navegador.findElement(
+//                    By.cssSelector("[data-test='social-linkedin']")
+//            );
+//            String linkedDestino = linkedinLink.getAttribute("href");
+//            System.out.println(linkedDestino);
+//            String tagName = linkedinLink.getTagName();
+//            assertEquals(tagName, "a");
+//
+//            WebElement facebookLink = navegador.findElement(
+//                    By.cssSelector("[data-test='social-facebook']")
+//            );
+//            String facebookDestino = facebookLink.getAttribute("href");
+//            System.out.println(facebookDestino);
+//            tagName = facebookLink.getTagName();
+//            assertEquals(tagName, "a");
+//
+//            navegador.navigate().refresh();
         } finally {
             navegador.quit();
         }
